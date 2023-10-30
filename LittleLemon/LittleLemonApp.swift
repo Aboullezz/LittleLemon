02cad3008d03d6b29528ef1f691a12fad0e05025
aboullezz <aboullezz@gmail.com>
2023-10-30 14:28:26 +0200

    first commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Mohamed Aboullezz on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
